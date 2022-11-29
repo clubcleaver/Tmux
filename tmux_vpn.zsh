bdd86010881797a0038e3d90fe7c60322fd39787
clubcleaver <101668450+clubcleaver@users.noreply.github.com>
2022-11-28 23:32:07 -0500

    Create tmux_vpn.zsh
    Creates a session for VPN and runs the main script after sleep 5.

---
+++ tmux_vpn.zsh
@@ -0,0 +1,8 @@
+#! /usr/bin/zsh
+tmux new -d -s vpn
+# need to edit this part to ask for the vpn file as an argument
+tmux send-keys -t 'vpn' "sudo openvpn ~/Downloads/pg.ovpn" C-m
+sleep 2
+tmux send-keys -t 'vpn' "kali" Enter
+sleep 5
+~/tmux_main.zsh
